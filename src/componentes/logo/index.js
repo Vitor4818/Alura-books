3fbcc69d83b3497ef525aae23be7b9e33950722e
Vitor4818 <vitorg.martins4818@gmail.com>
2024-06-28 19:57:43 -0300

    Estilizando com Styled component

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,12 +1,23 @@
-import './estilo.css'
 import logo from '../../imagens/logo.svg'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div`
+    display: flex;
+    font-size: 30px;
+    width: 20%;
+`
+
+const LogoImg = styled.img`
+    margin-right: 5%;
+`
+
 
 function Logo(){
  return(
-<div className='logo'>
-          <img src={logo} alt='logo' className='logo-img'></img>
-          <p><strong>Alura</strong>-Books</p>
-        </div>
+    <LogoContainer>
+      <LogoImg src={logo} alt='logo' className='logo-img'></LogoImg>
+      <p><strong>Alura</strong>-Books</p>
+    </LogoContainer>
  )   
 }
 export default Logo
